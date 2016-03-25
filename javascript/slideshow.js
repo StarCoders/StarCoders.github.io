9bc2165b801cc583849485402f08dc1d97c3963b
Professor Taki <Taki21@users.noreply.github.com>
2016-03-25 09:44:37 -0400

    Update slideshow.js

--- javascript/slideshow.js
+++ javascript/slideshow.js
@@ -1,1 +1,24 @@
-
+$(document).ready(function() {
+  $('img.p2').hide();
+  $('img.p3').hide();
+  $('img.p4').hide();
+  $('img.p5').hide();
+  $('img.p6').hide();
+  
+  $('img.p1').fadeOut(1000);
+  $('img.p2').fadeIn(1000);
+  
+  $('img.p2').fadeOut(1000);
+  $('img.p3').fadeIn(1000);
+  
+  $('img.p3').fadeOut(1000);
+  $('img.p4').fadeIn(1000);
+  
+  $('img.p4').fadeOut(1000);
+  $('img.p5').fadeIn(1000);
+  
+  $('img.p5').fadeOut(1000);
+  $('img.p6').fadeIn(1000);
+  
+  .appendTo('img.p1');
+});
