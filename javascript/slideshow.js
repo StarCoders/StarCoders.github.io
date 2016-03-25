01c01c369662c22c905a8a8e69def1ccbe636859
Professor Taki <Taki21@users.noreply.github.com>
2016-03-25 09:56:22 -0400

    Update slideshow.js

--- javascript/slideshow.js
+++ javascript/slideshow.js
@@ -1,24 +1,34 @@
 $(document).ready(function() {
   
   $('img.p1').hide();
-  $('img.p1').hide();
+  $('img.p2').hide();
+  $('img.p3').hide();
+  $('img.p4').hide();
+  $('img.p5').hide();
+  $('img.p6').hide();
   
-  $('img.p1').fadeIn(1000)
+  $('img.p1').show();
+  $('img.p1').fadeIn(1000);
   
-  $('img.p1').fadeOut(1000).next()
-  $('img.p2').fadeIn(1000)
+  $('img.p1').fadeOut(1000);
+  $('img.p2').show();
+  $('img.p2').fadeIn(1000);
   
-  $('img.p2').fadeOut(1000).next()
-  $('img.p3').fadeIn(1000)
+  $('img.p2').fadeOut(1000);
+  $('img.p3').show();
+  $('img.p3').fadeIn(1000);
   
-  $('img.p3').fadeOut(1000).next()
-  $('img.p4').fadeIn(1000)
+  $('img.p3').fadeOut(1000);
+  $('img.p4').show();
+  $('img.p4').fadeIn(1000);
   
-  $('img.p4').fadeOut(1000).next()
-  $('img.p5').fadeIn(1000)
+  $('img.p4').fadeOut(1000);
+  $('img.p5').show();
+  $('img.p5').fadeIn(1000);
   
-  $('img.p5').fadeOut(1000).next()
-  $('img.p6').fadeIn(1000)
+  $('img.p5').fadeOut(1000);
+  $('img.p6').show();
+  $('img.p6').fadeIn(1000);
   
   .appendTo('img.p1');
 });
