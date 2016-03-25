c15bbe0b3f420aa5bb35935871099a65c4538f90
Professor Taki <Taki21@users.noreply.github.com>
2016-03-25 09:50:56 -0400

    Update slideshow.js

--- javascript/slideshow.js
+++ javascript/slideshow.js
@@ -1,20 +1,20 @@
 $(document).ready(function() {
   
-  $('img.p1').fadeIn(1000);
+  $('img.p1').fadeIn(1000)
   
-  $('img.p1').fadeOut(1000);
-  $('img.p2').fadeIn(1000);
+  $('img.p1').fadeOut(1000).next()
+  $('img.p2').fadeIn(1000)
   
-  $('img.p2').fadeOut(1000);
-  $('img.p3').fadeIn(1000);
+  $('img.p2').fadeOut(1000).next()
+  $('img.p3').fadeIn(1000)
   
-  $('img.p3').fadeOut(1000);
-  $('img.p4').fadeIn(1000);
+  $('img.p3').fadeOut(1000).next()
+  $('img.p4').fadeIn(1000)
   
-  $('img.p4').fadeOut(1000);
-  $('img.p5').fadeIn(1000);
+  $('img.p4').fadeOut(1000).next()
+  $('img.p5').fadeIn(1000)
   
-  $('img.p5').fadeOut(1000);
+  $('img.p5').fadeOut(1000).next()
   $('img.p6').fadeIn(1000)
   
   .appendTo('img.p1');
