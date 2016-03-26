a62799d71c08b37048794d4f9580e51ffe1c9104
Professor Taki <Taki21@users.noreply.github.com>
2016-03-26 14:14:30 -0400

    Update transitions.js

--- javascript/transitions.js
+++ javascript/transitions.js
@@ -5,20 +5,4 @@ $(document).ready(function() {
   $('p.forums').fadeIn();
   $('p.vip').fadeIn();
   $('p.github').fadeIn();
-  
-  // Nav Hover Effects!
-  $('p.home').hover();
-  $(this).css('background-color', '#b3bfe7');
-  
-  $('p.news').hover();
-  $(this).css('background-color', '#b3bfe7');
-  
-  $('p.forums').hover();
-  $(this).css('background-color', '#b3bfe7');
-  
-  $('p.vip').hover();
-  $(this).css('background-color', '#b3bfe7');
-  
-  $('p.github').hover();
-  $(this).css('background-color', '#b3bfe7');
 });
