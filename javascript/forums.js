5a1268eec1bd8e27befaa8b3dca7b69d0ea4c53d
Professor Taki <Taki21@users.noreply.github.com>
2016-03-27 09:58:48 -0400

    Delete forums.js

--- javascript/forums.js
+++
@@ -1,9 +0,0 @@
-function post(){
-  $(document).ready(function() {
-    var name = $("input").val();
-    var comment = $("textarea").val();
-    $("button").click(function() {
-      $("<p><strong>" + name + "</strong> Said:<br>" + comment + "</p>").insertAfter("p.null");
-    })
-  });
-}
