ae1ba72bb7ec34fc5b1d5637a2e8c93a1720f278
Professor Taki <Taki21@users.noreply.github.com>
2016-03-26 20:16:33 -0400

    Update forums.js

--- javascript/forums.js
+++ javascript/forums.js
@@ -1,1 +1,9 @@
-
+function post(){
+  $(document).ready(function() {
+    var name = $("input").val();
+    var comment = $("textarea").val();
+    $("button").click(function() {
+      $("<p><strong>" + name "</strong> Said:<br>" + comment).insertAfter("p.null");
+    })
+  });
+}
